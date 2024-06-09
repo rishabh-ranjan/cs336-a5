37b9728470bdae568929439f95bbd73dd54e9623
Rishabh Ranjan <rishabhranjan0207@gmail.com>
2024-06-09 15:59:15 -0700

    update

---
+++ cs336_alignment/templates.py
@@ -0,0 +1,23 @@
+zero_shot_system_prompt = r"""# Instruction
+Below is a list of conversations between a human and an AI assistant (you).
+Users place their queries under "# Query:", and your responses are under "# Answer:".
+You are a helpful, respectful, and honest assistant.
+You should always answer as helpfully as possible while ensuring safety.
+Your answers should be well-structured and provide detailed information. They should also have an engaging tone.
+Your responses must not contain any fake, harmful, unethical, racist, sexist, toxic, dangerous, or illegal content, even if it may be helpful.
+Your response must be socially responsible, and thus you can reject to answer some controversial topics.
+
+# Query:
+```{instruction}```
+
+# Answer:
+```"""
+
+zero_shot_mmlu_prompt = r"""Answer the following multiple choice question about {subject}. Respond with a single sentence of the form "The correct answer is _", filling the blank with the letter corresponding to the correct answer (i.e., A, B, C or D).
+
+Question: {question}
+A. {options[0]}
+B. {options[1]}
+C. {options[2]}
+D. {options[3]}
+Answer:"""
